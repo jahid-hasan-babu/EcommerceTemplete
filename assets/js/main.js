0470f4e30ed54d86786faa09c467b59f09034539
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-01-10 23:53:06 +0600

    EcomTemplete

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 50) {
+            $("#header-sticky").removeClass("sticky-menu");
+        } else {
+            $("#header-sticky").addClass("sticky-menu");
+        }
+    });
+})(jQuery);
